5ff4545fcb824c71bf1f1ed22dcbb3dd5016b744
bereket <gezahegnebereket@gmail.com>
2025-11-10 15:08:44 +0300

    COrs dev

--- src/main/java/com/bereket/monitor/controller/MonitoredApiController.java
+++ src/main/java/com/bereket/monitor/controller/MonitoredApiController.java
@@ -3,6 +3,7 @@ package com.bereket.monitor.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.bereket.monitor.repo.MonitoredApiRepository;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v1/apis")
 @RequiredArgsConstructor
